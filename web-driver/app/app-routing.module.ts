84c63c22a3110984325ebcda0bc61ae22c0bb3ab
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-05-09 05:20:51 +0100

    Fix bug sur le lancement de l'application web-driver, dont les assets n'étaits pas configurés convénablement

--- web-driver/app/app-routing.module.ts
+++ web-driver/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'web-driver',
+    component: AppComponent,
+    children: [
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {})],
