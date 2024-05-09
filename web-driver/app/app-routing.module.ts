bef6d7bbf6f6f31f668ad4d3f3c22323a94b3bd9
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-05-09 05:43:58 +0100

    Mettre en place une logique de définitions des alias de l'application

--- web-driver/app/app-routing.module.ts
+++ web-driver/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+import { AppComponent } from '$wd/app.component';
 
 const routes: Routes = [
   {
