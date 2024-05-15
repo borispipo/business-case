1b3729ff69583e4bfba862a838f4e0c9d6a15867
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-05-15 07:56:23 +0100

    Début prise en compte des évènements sockets avec possibilité de garder les connections actives

--- client/shared/components/socket/socket.component.ts
+++ client/shared/components/socket/socket.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {connect} from "./utils";
+import {connect,sendMessage} from "./utils";
 import BaseComponent from '../base';
+const {delivery_updated,location_changed,status_changed} = require("$socket-events");
 @Component({
   selector: 'app-socket',
   standalone: true,
@@ -10,8 +11,33 @@ import BaseComponent from '../base';
 })
 export class SocketComponent extends BaseComponent{
   socket = null;
+  readonly clientId = this.uniqid("socket-client-id");
+  isConnected : boolean = false;
+  init(){
+    console.log("initiallizing socketttttt");
+    this.socket = connect({
+      clientId : this.clientId,
+      onOpen : ()=>{
+        this.isConnected = true;
+        this.sendMessage(delivery_updated);
+      },
+      onClose : ()=>{
+        console.log(this.clientId," socket connection closed");
+        this.isConnected = false;
+      }
+    });
+  }
+  sendMessage(type:string, options = null){
+      options = Object.assign({},options);
+      options.clientId = this.clientId
+      return sendMessage(this.socket,type,options);
+  }
   ngAfterViewInit(): void {
-    this.socket = connect();
-    
+    this.init();
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.socket?.disconnect();
   }
 }
--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,13 @@ const { Server } = require("socket.io");
 const PINGINTERVAL = 5000;
 const PINGTIMEOUT = 7100;
 const events = require("./events");
+const eventEmitter = require("../events");
+
+const clients = {};
+const getQueryParams = (socket)=>{
+    return Object.assign({},socket?.handshake?.query);
+}
+const getClientId = (socket)=> getQueryParams(socket).clientId;
 
 const ioInstance = {current : null};
 module.exports = {
@@ -18,11 +25,23 @@ module.exports = {
             });
             //on socket io connection opened
             io.on("connection", (socket) => {
-                console.log("a client is connected");
+                const params = getQueryParams(socket);
+                const clientId = params.clientId;
+                if(!clientId){
+                    socket.emit("INVALID_CONNECTION",JSON.stringify(params));
+                    socket.disconnect();
+                    return;
+                }
+                clients [clientId] = {socket};
+                console.log(`socket client with id ${clientId} is connected`);
                 //socket.on(types.GET_GURUX_APP_CONNECT, authCtrl.getGuruxAppConnect);
                 //socket.on(types.RESTART_APP, updateCtrl.restartApp);
-                socket.on('disconnect', () => {
-                    console.log('user disconnected');
+                socket.on('disconnect', (...rest) => {
+                    console.log('socket client disconnected',rest);
+                    delete clients[clientId];
+                });
+                socket.on(events.delivery_updated,(...rest)=>{
+                    console.log("delivery changeddd ",rest,clients);
                 });
                 socket.on(events.location_changed,(data,event)=>{
                     console.log("location change for ",data,event);
@@ -30,12 +49,20 @@ module.exports = {
                 socket.on(events.status_changed,(data,event)=>{
                     console.log("delivery status change for ",data,event);
                 });
+                
+            });
+            eventEmitter.addListener(eventEmitter.SOCKET_EVENT,function(data){
+                    
             });
             ioInstance.current = io;
+            
             return io;
         }
     },
     get io (){
         return ioInstance.current;       
     }
+    
 }
+
+module.exports.clients = {};
