e71dce384b27fdea44b581796aede8b970f6f507
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-05-15 06:21:51 +0100

    Implémenter la fonction de direction sur le map Faire en sorte que l'on puisse zoomer le map, diminuer les directions et bien d'autres

--- client/shared/components/map/map.component.ts
+++ client/shared/components/map/map.component.ts
@@ -1,41 +1,51 @@
+import { MatDialog } from '@angular/material/dialog';
 //@see : https://github.com/angular/components/blob/main/src/google-maps/README.md
 import {Component, Input, ViewChild} from '@angular/core';
+import { Observable,map } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import {GoogleMapsModule, MapInfoWindow, MapMarker,MapAdvancedMarker} from "@angular/google-maps";
+import {GoogleMapsModule, MapInfoWindow, MapMarker,MapDirectionsRenderer, MapDirectionsService} from "@angular/google-maps";
 import Location from '$shared/types/Location';
 import BaseComponent from '../base';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [CommonModule,GoogleMapsModule],
+  imports: [CommonModule,GoogleMapsModule,MapDirectionsRenderer],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
 export class MapComponent extends BaseComponent{
   
   @ViewChild(MapInfoWindow, { static: false }) infoWindow?: MapInfoWindow;
-
   @Input() locationFrom?: Location;
   @Input() locationTo?: Location;
   @Input() location ? : Location;
+  directionsResults$: Observable<google.maps.DirectionsResult|undefined> = null;
+  mapDirectionsService : MapDirectionsService;
 
   @Input() !height: string = undefined;
   @Input() !width: string  = undefined;
   readonly markersPositions : Location[] = [];
-
+  constructor(dialog:MatDialog,confirmDialog : MatDialog,mapDirectionsService: MapDirectionsService) {
+    super(dialog,confirmDialog);
+    this.mapDirectionsService = mapDirectionsService;
+  }
+  get mapId() : string{
+    return "custom-map-id";
+  }
   @Input() !mapOptions: google.maps.MapOptions = {
     center: {
       lat: this.location?.lat || 0,
       lng: this.location?.lng || 0
     },
-    mapId: 'customMap',
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
+    mapId: this.mapId,
+    //scrollwheel: false,
+    //disableDoubleClickZoom: true,
     mapTypeId: 'hybrid',
+    scaleControl: true,
     zoom: 6,
-    maxZoom: 15,
-    minZoom: 4,
+    //maxZoom: 15,
+    //minZoom: 4,
   };
   infoContent: string = '';
 
@@ -66,15 +76,41 @@ export class MapComponent extends BaseComponent{
       }
     });
   }
-
+  prefixLocationTitle(location:Location,title:string) : void{
+    if(location && title){
+      location.title = location.title || '';
+      if(!location.title.toLowerCase().startsWith(title.toLowerCase().trim())){
+        location.title = `${title} : ${location.title}`;
+      }
+    }
+  }
+  prepareDirections(){
+    if(this.locationFrom && this.locationTo){
+      const request: google.maps.DirectionsRequest = {
+        destination: this.locationTo,
+        origin: this.locationFrom,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+      this.directionsResults$ = this.mapDirectionsService.route(request).pipe(map(response => response.result));
+    }
+  }
   ngOnChanges(): void {
+    this.prepareDirections();
+    this.prefixLocationTitle(this.locationFrom,'From');
+    this.prefixLocationTitle(this.locationTo, ' To');
+    this.prefixLocationTitle(this.location,"Current location");
     this.locations.map(this.addMarkerPostion.bind(this));
     if (this.hasLocation) {
       this.addPolyline();
     }
   }
+  get locationsObjects(){
+    return {location:this.location,locationFrom:this.locationFrom,locationTo:this.locationTo};
+  }
   get locations() : Location[]{
-    return [this.locationFrom,this.locationTo,this.location];
+    return ["locationFrom","location","locationTo"].map((key)=>{
+      return this.locationsObjects[key];
+    });
   }
   get hasLocation(): boolean {
     return this.locations.filter((l)=>!!l).length >=2;
@@ -109,7 +145,7 @@ export class MapComponent extends BaseComponent{
   addPolyline(): void {
     const path: google.maps.LatLng[] = [];
     this.markersPositions.forEach((position, index) => {
-      path.push(new google.maps.LatLng(position!));
+        path.push(new google.maps.LatLng(position!));
     });
     this.polylineOptions = { ...this.polylineOptions, path };
   }
